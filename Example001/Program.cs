a176a5f94404de85754e73ca81d9e5f5ec93a79a
Michail <miha.filippov.10@mail.ru>
2022-09-05 00:06:26 +0800

    Example001_ver1

---
+++ Example001/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] arrayFirst = new string[6] {"546", "pepega", "Kart_racing", "Pit", "99", "Final_Countdown"};
+string[] arraySecond = new string[arrayFirst.Length];
+int count = 0;
+for (int i = 0; i < arrayFirst.Length; i++)
+{
+if(arrayFirst[i].Length <= 3)
+    {
+    arraySecond[count] = arrayFirst[i];
+    count++;
+    }
+}
+for (int i = 0; i < arraySecond.Length; i++)
+{
+    Console.Write($"{arraySecond[i]} ");
+}
+Console.WriteLine();
